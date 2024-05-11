9f5ffc28a3f66949263c509f4b0023f758d5c466
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2024-05-11 15:21:47 +0300

    fix: import lazygit

--- config/default.nix
+++ config/default.nix
@@ -1,28 +1,29 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   # Import all your configuration modules here
-  imports = [ ./plugins ];
+  imports = [
+    ./plugins
+  ];
   config = {
-
     globals.mapleader = " ";
     clipboard.register = "unnamedplus";
     colorschemes.catppuccin.enable = true;
   };
-    options = {
-      number = true;
-      breakindent = true;
-      shiftwidth = 2;
-      relativenumber = true;
-      cmdheight = 0;
-      fillchars.eob = " ";
-      scrolloff = 1000;
-      foldenable = true;
-      foldlevel = 99;
-      foldlevelstart = 99;
-      writebackup = false;
-      virtualedit = "block";
-      undofile = true;
-      timeoutlen = 500;
-      termguicolors = true;
-      smartindent = true;
-    };
+  options = {
+    number = true;
+    breakindent = true;
+    shiftwidth = 2;
+    relativenumber = true;
+    cmdheight = 0;
+    fillchars.eob = " ";
+    scrolloff = 1000;
+    foldenable = true;
+    foldlevel = 99;
+    foldlevelstart = 99;
+    writebackup = false;
+    virtualedit = "block";
+    undofile = true;
+    timeoutlen = 500;
+    termguicolors = true;
+    smartindent = true;
+  };
 }
